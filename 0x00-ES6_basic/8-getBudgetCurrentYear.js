d4dd58ca7aba97826a5ecedc7947e598de434536
lavendahkinyua <lavendah17@gmail.com>
2023-07-31 15:29:43 +0300

    commit corrections

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,9 +4,9 @@ function getCurrentYear() {
 }
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    [`income-${getCurrentYear()}`] : income,
-    [`gd-p-${getCurrentYear()}`] : gdp,
-    [`capita-${getCurrentYear()}`] : capita,
+    [`income-${getCurrentYear()}`]: income,
+    [`gd-p-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
   return budget;
 }
