3f2b27babe0918c68747ccc4b7a6ee3bb395e8c4
lavendahkinyua <lavendah17@gmail.com>
2023-07-31 17:06:13 +0300

    commit corrections

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   return (function* _() {
     for (const department of Object.values(report.allEmployees)) {
-    for (const employee of department) {
+      for (const employee of department) {
         yield employee;
       }
     }
