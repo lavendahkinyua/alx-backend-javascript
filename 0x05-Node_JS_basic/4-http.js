2333631f6b52fac752e1aeec3697f8d7ba0e957b
lavendahkinyua <lavendah17@gmail.com>
2023-10-23 21:48:56 +0300

    task commit 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,3 +1,20 @@
 /** 
+ * creating a small HTTP server using Node's HTTP module
  * 
  */
+const http = require('http');
+
+const port = 1245;
+const host = 'localhost';
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
+});
+
+module.exports = app;
