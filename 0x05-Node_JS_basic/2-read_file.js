c162103dea99d0eb01ee2f06280d845e49107e35
lavendahkinyua <lavendah17@gmail.com>
2023-10-23 21:14:01 +0300

    commit task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,27 +5,27 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-    let data;
+  let data;
 
-    try {
-        data = fs.readFileSync(path, 'utf8');
-    } catch (err) {
-        throw new Error('Cannot load the database');
-    }
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 
-    data = data.split('\n');
+  data = data.split('\n');
 
-    let students = data.filter((line) => line)
+  let students = data.filter((line) => line)
 
-    .map((line) => line.split(','))
-        .filter((student) => student.length === 4 && student[0] !== 'firstname')
-        .map((student) => student[3]);
+  .map((line) => line.split(','))
+    .filter((student) => student.length === 4 && student[0] !== 'firstname')
+    .map((student) => student[3]);
 
-    console.log(`Number of students: ${students.length}`);
-    let csStudents = students.filter((student) => student.includes('CS'));
-    let sweStudents = students.filter((student) => student.includes('SWE'));
-    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  console.log(`Number of students: ${students.length}`);
+  let csStudents = students.filter((student) => student.includes('CS'));
+  let sweStudents = students.filter((student) => student.includes('SWE'));
+  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
 }
 
 module.exports = countStudents;
