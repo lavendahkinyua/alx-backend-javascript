8483c513beb8d31eb018b448e8e4b1a436276b82
lavendahkinyua <lavendah17@gmail.com>
2023-10-23 21:17:40 +0300

    task commit 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -15,17 +15,17 @@ const countStudents = (path) => {
 
   data = data.split('\n');
 
-  let students = data.filter((line) => line)
+  const students = data.filter((line) => line)
 
-  .map((line) => line.split(','))
+    .map((line) => line.split(','))
     .filter((student) => student.length === 4 && student[0] !== 'firstname')
     .map((student) => student[3]);
 
   console.log(`Number of students: ${students.length}`);
-  let csStudents = students.filter((student) => student.includes('CS'));
-  let sweStudents = students.filter((student) => student.includes('SWE'));
+  const csStudents = students.filter((student) => student.includes('CS'));
+  const sweStudents = students.filter((student) => student.includes('SWE'));
   console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
   console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-}
+};
 
 module.exports = countStudents;
