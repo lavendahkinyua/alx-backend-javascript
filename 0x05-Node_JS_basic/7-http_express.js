c1c7479bcafe19a1f1e0da2691f99111582277c0
lavendahkinyua <lavendah17@gmail.com>
2023-10-23 23:54:38 +0300

    commit task

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -6,31 +6,31 @@ const PORT = 1245;
 const database = process.argv[2];
 
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
-    if (!dataPath) {
-      reject(new Error('Cannot load the database'));
-    }
-    if (dataPath) {
-      fs.readFile(dataPath, (err, data) => {
-        if (err) {
-          reject(new Error('Cannot load the database'));
-        }
-        if (data) {
-          const reportParts = [];
-          const fileLines = data.toString('utf-8').trim().split('\n');
-          const studentGroups = {};
-          const dbFieldNames = fileLines[0].split(',');
-          const studentPropNames = dbFieldNames.slice(
-            0,
-            dbFieldNames.length - 1,
-          );
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (dataPath) {
+    fs.readFile(dataPath, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (data) {
+        const reportParts = [];
+        const fileLines = data.toString('utf-8').trim().split('\n');
+        const studentGroups = {};
+        const dbFieldNames = fileLines[0].split(',');
+        const studentPropNames = dbFieldNames.slice(
+          0,
+          dbFieldNames.length - 1,
+        );
   
-          for (const line of fileLines.slice(1)) {
-            const studentRecord = line.split(',');
-            const studentPropValues = studentRecord.slice(
-              0,
-              studentRecord.length - 1,
-            );
-            const field = studentRecord[studentRecord.length - 1];
+        for (const line of fileLines.slice(1)) {
+          const studentRecord = line.split(',');
+          const studentPropValues = studentRecord.slice(
+            0,
+            studentRecord.length - 1,
+          );
+          const field = studentRecord[studentRecord.length - 1];
             if (!Object.keys(studentGroups).includes(field)) {
               studentGroups[field] = [];
             }
