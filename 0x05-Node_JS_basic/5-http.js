4697535de62b12d683022745ec95dea9396edaef
lavendahkinyua <lavendah17@gmail.com>
2023-10-23 22:57:28 +0300

    task commit

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -8,26 +8,26 @@ const port = 1245;
 const host = 'localhost';
 
 const app = http.createServer((req, res) => {
-    if (req.url === '/') {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(process.argv[2])
+      .then((data) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-        countStudents(process.argv[2])
-        .then((data) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(`This is the list of our students\n${data}`);
-        })
-        .catch((error) => {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(error.message);
-        });
-    }
-    });
+        res.end(`This is the list of our students\n${data}`);
+      })
+      .catch((error) => {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(error.message);
+      });
+  }
+});
 app.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}`);
+  console.log(`Server running at http://${host}:${port}`);
 });
 
 module.exports = app;
