9520a3b0333c7683252125958b6e45585511ff29
lavendahkinyua <lavendah17@gmail.com>
2023-10-27 01:26:34 +0300

    commit task

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,58 +1,36 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul').default;
+const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-  describe('Two Integers', function () {
-    it('should return 4', function () {
-      assert.strictEqual(calculateNumber(1, 3), 4);
-    });
+describe('calculateNumber', () => {
+  it('floating point whole numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
 
-  describe('One round', function () {
-    it('should return 5', function () {
-      assert.strictEqual(calculateNumber(1, 3.7), 5);
-    });
+  it('rounding down b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
 
-  describe('One round 2', function () {
-    it('should return 5', function () {
-      assert.strictEqual(calculateNumber(3.7, 1), 5);
-    });
+  it('rounding down a and b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
   });
 
-  describe('One round floor', function () {
-    it('should return 4', function () {
-      assert.strictEqual(calculateNumber(1, 3.3), 4);
-    });
+  it('rounding down a\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
   });
 
-  describe('One round floor 2', function () {
-    it('should return 4', function () {
-      assert.strictEqual(calculateNumber(3.3, 1), 4);
-    });
+  it('rounding up b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
   });
 
-  describe('Two round', function () {
-    it('should return 5', function () {
-      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    });
+  it('rounding up a and b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
   });
 
-  describe('Two round again', function () {
-    it('should return 6', function () {
-      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    });
+  it('rounding up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
   });
 
-  describe('Two round reversed', function () {
-    it('should return 5', function () {
-      assert.strictEqual(calculateNumber(3.7, 1.2), 5);
-    });
-  });
-
-  describe('Two round again floor both', function () {
-    it('should return 3', function () {
-      assert.strictEqual(calculateNumber(1.2, 2.1), 3);
-    });
+  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
