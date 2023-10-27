b1b03f813c9458ca7755dd51063e08740682fa4b
lavendahkinyua <lavendah17@gmail.com>
2023-10-27 11:36:25 +0300

    lint changes

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,17 +1,17 @@
-import AppController from "../controllers/AppController";
-import StudentsController from "../controllers/StudentsController";
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
 
 const mapRoutes = (app) => {
-    app.get("/", (request, response) => {
-        AppController.getHomepage(request, response);
-    });
-    app.get("/students", (request, response) => {
-        StudentsController.getAllStudents(request, response);
-    });
-    app.get("/students/:major", (request, response) => {
-        StudentsController.getAllStudentsByMajor(request, response);
-    });
-    }
+  app.get('/', (request, response) => {
+    AppController.getHomepage(request, response);
+  });
+  app.get('/students', (request, response) => {
+    StudentsController.getAllStudents(request, response);
+  });
+  app.get('/students/:major', (request, response) => {
+    StudentsController.getAllStudentsByMajor(request, response);
+  });
+};
 
 export default mapRoutes;
 module.exports = mapRoutes;
