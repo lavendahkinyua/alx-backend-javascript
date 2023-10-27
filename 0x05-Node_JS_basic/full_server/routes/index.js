881d3fcc3e8a615e19ff553e7851ab69de5e2afe
lavendahkinyua <lavendah17@gmail.com>
2023-10-27 11:58:31 +0300

    task commit

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -2,16 +2,10 @@ import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
 const mapRoutes = (app) => {
-  app.get('/', (request, response) => {
-    AppController.getHomepage(request, response);
-  });
-  app.get('/students', (request, response) => {
-    StudentsController.getAllStudents(request, response);
-  });
-  app.get('/students/:major', (request, response) => {
-    StudentsController.getAllStudentsByMajor(request, response);
-  });
-};
-
-export default mapRoutes;
-module.exports = mapRoutes;
+    app.get('/', AppController.getHomepage);
+    app.get('/students', StudentsController.getAllStudents);
+    app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+  };
+  
+  export default mapRoutes;
+  module.exports = mapRoutes;
