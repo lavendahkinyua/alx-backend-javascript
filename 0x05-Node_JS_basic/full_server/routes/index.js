c6d43df8d05a53d028d90ad1b75e1c234014c98b
lavendahkinyua <lavendah17@gmail.com>
2023-10-27 12:03:49 +0300

    corrections

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -2,10 +2,10 @@ import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
 const mapRoutes = (app) => {
-    app.get('/', AppController.getHomepage);
-    app.get('/students', StudentsController.getAllStudents);
-    app.get('/students/:major', StudentsController.getAllStudentsByMajor);
-  };
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
   
-  export default mapRoutes;
-  module.exports = mapRoutes;
+export default mapRoutes;
+module.exports = mapRoutes;
