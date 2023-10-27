b973b0b95606602717e850dad65642d36b07cffc
lavendahkinyua <lavendah17@gmail.com>
2023-10-27 11:02:25 +0300

    task 5 changes

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,32 +2,59 @@
  * create a small HTTP server using Node's HTTP module
  */
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+const { argv } = require('process');
 
+function countStudents(path, stream) {
+  if (fs.existsSync(path)) {
+    const data = fs.readFileSync(path, 'utf8');
+    const result = [];
+    data.split('\n').forEach((data) => {
+      result.push(data.split(','));
+    });
+    result.shift();
+    const newis = [];
+    result.forEach((data) => newis.push([data[0], data[3]]));
+    const fields = new Set();
+    newis.forEach((item) => fields.add(item[1]));
+    const final = {};
+    fields.forEach((data) => { (final[data] = 0); });
+    newis.forEach((data) => { (final[data[1]] += 1); });
+    stream.write(`Number of students: ${result.length}\n`);
+    const temp = [];
+    Object.keys(final).forEach((data) => temp.push(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}\n`));
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < temp.length; i++) {
+      if (i === temp.length - 1) {
+        temp[i] = temp[i].replace(/(\r\n|\n|\r)/gm, '');
+      }
+      stream.write(temp[i]);
+    }
+  } else { throw new Error('Cannot load the database'); }
+}
+
+const hostname = 'localhost';
 const port = 1245;
-const host = 'localhost';
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then((data) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch((error) => {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(error.message);
-      });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  const { url } = req;
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      countStudents(argv[2], res);
+      res.end();
+    } catch (err) {
+      res.end(err.message);
+    }
   }
 });
-app.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}`);
-});
+
+app.listen(port, hostname);
 
 module.exports = app;
